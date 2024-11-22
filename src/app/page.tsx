42cc9f755b103b5621a503ab66d26a3a2fd6330e
Patriksafar <patrik.safar@educanet.cz>
2024-11-22 22:13:06 +0100

    feat: edit orbit controls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera, SoftShadows } from '@react-three/drei'
 
 import MedievalLand from '@/components/models/medieval-land'
+import { MathUtils } from 'three'
 
 export default function Component() {
   return (
@@ -23,7 +24,10 @@ export default function Component() {
           focus={0.5}
         />
         <PerspectiveCamera makeDefault position={[0, 20, 25]} />
-        <OrbitControls enableZoom={true} />
+        <OrbitControls enableZoom={true}
+          minPolarAngle={MathUtils.degToRad(20)} // Restrict to 0 degrees (top view)
+          maxPolarAngle={MathUtils.degToRad(80)} // Restrict to 80 degrees (slightly above horizon)
+        />
         <ambientLight intensity={0.4} />
         <directionalLight 
           position={[-10, 15, -10]} 
